234115768c976c90397b456d41d6e480779130b6
Diogo Ferraz <Diogo.Ferraz@vision-box.com>
2019-11-05 15:59:47 +0000

    Improvement of state for app, and style

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ const initialState = {
     { id: 2, ball: false },
     { id: 3, ball: false }
   ],
-  message: ''
+  message: { text: 'Take a close look where the black ball is, after it click on Start!', type: 'info' },
+  overlay: false,
+  hideImage: false,
+  startRunning: false,
 };
 
 class App extends Component {
@@ -20,56 +23,52 @@ class App extends Component {
     this.numFlipKey = this.numFlipKey.bind(this);
     this.isTheOne = this.isTheOne.bind(this);
     this.onShuffleStart = this.onShuffleStart.bind(this);
-    this.onStopShuffle = this.onStopShuffle.bind(this);
-    this.hideImage = false;
+    this.onShuffleStop = this.onShuffleStop.bind(this);
     this.count = 0;
   }
 
   numFlipKey = () => Math.floor(Math.random() * 100) + 1;
 
   isTheOne = (e) => {
-    if(!this.startRuning) return;
+    if (!this.state.startRunning) return;
     const choosen = _.find(this.state.data, item => item.ball === true);
     switch (choosen.id === e) {
       case true:
-        this.setState({ message: 'You got it! Congratulations!' });
-        this.hideImage = false;
+        this.setState({ message: { text: 'You got it! Congratulations!', type: 'success' }, hideImage: false });
         break;
       default:
-        this.setState({ message: 'Swing and a miss! Try again!' });
+        this.setState({ message: { text: 'Swing and a miss! Try again!', type: 'error' } });
         break;
     }
   };
 
   onShuffleStart = () => {
-    this.hideImage = true;
-    this.startRuning = true;
     this.count = 0;
     this.interval = setInterval(() => {
-      this.setState({ data: _.shuffle(this.state.data) });
+      this.setState({ data: _.shuffle(this.state.data), overlay: true, message: { text: 'Shuffling...', type: 'info' }, hideImage: true, startRunning: true });
     }, 500);
   };
 
   onRetry = () => {
-    this.hideImage = false;
-    this.startRuning = false;
     const randomBall = Math.floor(Math.random() * 3) + 1;
     const newData = [];
 
-    _.forEach(this.state.data, (item) => {
-        if(item.id === randomBall) {
-          item.ball = true;
-        } else {
-          item.ball = false;
-        }
-        newData.push(item);
+    _.forEach(this.state.data, (item) => {
+      if (item.id === randomBall) {
+        item.ball = true;
+      } else {
+        item.ball = false;
+      }
+      newData.push(item);
     });
-    this.setState({data: newData, message: ''});
+
+    this.setState({ data: newData, message: { text: 'Take a close look where the black ball is, after it click on Start!', type: 'info' }, overlay: true, hideImage: false, startRunning: false });
   }
 
-  onStopShuffle = () => {
+  onShuffleStop = () => {
     clearInterval(this.interval);
     this.interval = undefined;
+    this.setState({ overlay: false, message: { text: 'Click on the rectangle which hides the black ball', type: 'info' } });
   }
 
   componentWillUnmount() {
@@ -77,23 +76,25 @@ class App extends Component {
   }
 
   render() {
+    const { message, overlay, data, hideImage, startRunning } = this.state;
     return (
       <div className={'wrapper'}>
         <div className={'header'}>
-          {this.state.message && <div className={'message'}>{this.state.message}</div>}
+          {message.text && <div className={`message ${message.type}`}><p>{message.text}</p></div>}
         </div>
         <div className={'content'}>
+          {overlay && <div className={'overlay'} onClick={(e) => console.log(e)} />}
           <Flipper flipKey={this.numFlipKey()}>
             <ul className={'list'}>
-              {this.state.data.map(d => (
+              {data.map(d => (
                 <Flipped key={d.id} flipId={d.id}
                   onComplete={() => {
                     this.count = _.add(this.count, 1);
-                    this.count >= 20 && this.onStopShuffle();
+                    if (this.count === 20) this.onShuffleStop();
                   }}>
                   <li key={d.id}>
                     <div key={d.id} onClick={e => this.isTheOne(d.id)} className={'shell'} >
-                      {d.ball && <img src={Circle} className={`image ${this.hideImage ? 'hide' : ''}`} alt={''} />}
+                      {d.ball && <img src={Circle} className={`image ${hideImage ? 'hide' : ''}`} alt={''} />}
                     </div>
                   </li>
                 </Flipped>
@@ -102,8 +103,8 @@ class App extends Component {
           </Flipper>
         </div>
         <div className={'footer'}>
-          <button className={`button ${this.startRuning ? 'disabled': ''}`} onClick={this.onShuffleStart} disabled={this.startRuning}>Start</button>
-          <button className={'button'} onClick={this.onRetry}>Retry</button>
+          <button className={`button ${startRunning ? 'disabled' : ''}`} onClick={this.onShuffleStart} disabled={startRunning}>Start</button>
+          <button className={'button'} onClick={this.onRetry}>Restart</button>
         </div>
       </div>
     );
