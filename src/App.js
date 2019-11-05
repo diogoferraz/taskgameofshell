f879f229bf8d3174907fa7d2cb4b39cede4f7cfd
Diogo Ferraz <Diogo.Ferraz@vision-box.com>
2019-11-05 12:31:23 +0000

    Implement SCSS, change component from functinal to class

--- src/App.js
+++ src/App.js
@@ -1,70 +1,100 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { Component } from 'react';
 import { Flipper, Flipped } from 'react-flip-toolkit';
 import _ from 'lodash';
-import './assets/styles.css';
+import './assets/styles.scss';
 import Circle from './assets/images/circle.svg';
-const shells = [
-  { id: 1, ball: true },
-  { id: 2, ball: false },
-  { id: 3, ball: false },
-]
-const App = () => {
-  const numShuffle = () => Math.floor(Math.random() * 9) + 1;
-  const image = useRef();
-  const [data, setData] = useState(shells);
-  const [countShuffle, setCountShuffle] = useState(0);
-  const [showMessage, setShowMessage] = useState('');
-  let interval;
 
-  const isTheOne = (e) => {
+const initialState = {
+  data: [
+    { id: 1, ball: true },
+    { id: 2, ball: false },
+    { id: 3, ball: false }
+  ],
+  message: ''
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+    this.numFlipKey = this.numFlipKey.bind(this);
+    this.isTheOne = this.isTheOne.bind(this);
+    this.onShuffleStart = this.onShuffleStart.bind(this);
+    this.onStopShuffle = this.onStopShuffle.bind(this);
+    this.hideImage = false;
+    this.count = 0;
+  }
+
+  numFlipKey = () => Math.floor(Math.random() * 9) + 1;
+
+  isTheOne = (e) => {
     switch (e) {
-      case true:
-        setShowMessage('You got it! Congratulations!');
+      case 1:
+        this.setState({ message: 'You got it! Congratulations!' });
+        this.hideImage = false;
         break;
       default:
-        setShowMessage('Swing and a miss! Click on retry to try again!')
+        this.setState({ message: 'Swing and a miss! Try again!' });
         break;
     }
   };
 
-  const shuffleStart = () => {
-    interval = setInterval(() => {
-      if(countShuffle < 8) setData(_.shuffle(data));
+  onShuffleStart = () => {
+    this.hideImage = true;
+    this.startDisabled = true;
+    this.count = 0;
+    this.interval = setInterval(() => {
+      this.setState({ data: _.shuffle(this.state.data) });
     }, 500);
-
-    return () => clearInterval(interval);
   };
 
-  useEffect(() => {
-    shuffleStart();
-  }, []);
+  onRetry = () => {
+    this.hideImage = false;
+    this.startDisabled = false;
+    this.setState(initialState);
+  }
 
-  return (
-    <div className={'wrapper'}>
-      <div className={'header'}>
-        {showMessage && <div className={'message'}>{showMessage}</div>}
+  onStopShuffle = () => {
+    clearInterval(this.interval);
+    this.interval = undefined;
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className={'wrapper'}>
+        <div className={'header'}>
+          {this.state.message && <div className={'message'}>{this.state.message}</div>}
+        </div>
+        <div className={'content'}>
+          <Flipper flipKey={this.numFlipKey()}>
+            <ul className={'list'}>
+              {this.state.data.map(d => (
+                <Flipped key={d.id} flipId={d.id}
+                  onComplete={() => {
+                    this.count = _.add(this.count, 1);
+                    this.count >= 20 && this.onStopShuffle();
+                  }}>
+                  <li>
+                    <div onClick={e => this.isTheOne(d.id)} className={'shell'}>
+                      {d.ball && <img src={Circle} className={`image ${this.hideImage ? 'hide' : ''}`} alt={''} />}
+                    </div>
+                  </li>
+                </Flipped>
+              ))}
+            </ul>
+          </Flipper>
+        </div>
+        <div className={'footer'}>
+          <button className={`button ${this.startDisabled ? 'disabled': ''}`} onClick={this.onShuffleStart} disabled={this.startDisabled}>Start</button>
+          <button className={'button'} onClick={this.onRetry}>Retry</button>
+        </div>
       </div>
-      <div className={'content'}>
-        <Flipper flipKey={numShuffle()}>
-          <ul className={'list'}>
-            {data.map(d => (
-              <Flipped key={d.id} flipId={d.id} onComplete={() => {}}>
-                <li>
-                  <div onClick={e => isTheOne(d.id)} className={'shell'}>
-                    {d.ball && <img src={Circle} className={'image'} alt={''} ref={image} />}
-                  </div>
-                </li>
-              </Flipped>
-            ))}
-          </ul>
-        </Flipper>
-      </div>
-      <div className={'footer'}>
-        <button className={'button'} onClick={()=>{}}>Start</button>
-        <button className={'button'} onClick={() => {}}>Retry</button>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default App;
